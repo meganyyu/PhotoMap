bfd6d74fc95a1fd1bff1f43075eb8791f91aaadd
Megan Yu <meganyu@fb.com>
2020-07-08 09:19:10 -0700

    Added pins to map view

--- PhotoMap/View Controllers/PhotoMapViewController.m
+++ PhotoMap/View Controllers/PhotoMapViewController.m
@@ -30,6 +30,9 @@ static NSString *const kTagSegueID = @"tagSegue";
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    
+    _mapView.delegate = self;
+    
     //one degree of latitude is approximately 111 kilometers (69 miles) at all times.
     MKCoordinateRegion sfRegion = MKCoordinateRegionMake(CLLocationCoordinate2DMake(37.783333, -122.416667), MKCoordinateSpanMake(0.1, 0.1));
     [self.mapView setRegion:sfRegion animated:false];
@@ -57,6 +60,8 @@ static NSString *const kTagSegueID = @"tagSegue";
     [self presentViewController:imagePickerVC animated:YES completion:nil];
 }
 
+#pragma mark - UIImagePickerControllerDelegate
+
 - (void)imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary<UIImagePickerControllerInfoKey,id> *)info {
     // Get the image captured by the UIImagePickerController
     UIImage *originalImage = info[UIImagePickerControllerOriginalImage];
@@ -70,8 +75,33 @@ static NSString *const kTagSegueID = @"tagSegue";
     [self dismissViewControllerAnimated:YES completion:nil];
 }
 
+#pragma mark - LocationsViewControllerDelegate
+
 - (void)locationsViewController:(LocationsViewController *)controller didPickLocationWithLatitude:(NSNumber *)latitude longitude:(NSNumber *)longitude {
+    CLLocationCoordinate2D coordinate = CLLocationCoordinate2DMake(latitude.floatValue, longitude.floatValue);
+    
+    MKPointAnnotation *annotation = [MKPointAnnotation new];
+    annotation.coordinate = coordinate;
+    annotation.title = @"Picture!";
+    [self.mapView addAnnotation:annotation];
+    
     [self.navigationController popToViewController:self animated:YES];
+}
+
+#pragma mark - MapView
+
+- (MKAnnotationView *)mapView:(MKMapView *)mapView viewForAnnotation:(id<MKAnnotation>)annotation {
+    MKPinAnnotationView *annotationView = (MKPinAnnotationView*)[mapView dequeueReusableAnnotationViewWithIdentifier:@"Pin"];
+    if (annotationView == nil) {
+        annotationView = [[MKPinAnnotationView alloc] initWithAnnotation:annotation reuseIdentifier:@"Pin"];
+        annotationView.canShowCallout = true;
+        annotationView.leftCalloutAccessoryView = [[UIImageView alloc] initWithFrame:CGRectMake(0.0, 0.0, 50.0, 50.0)];
+    }
+
+    UIImageView *imageView = (UIImageView*)annotationView.leftCalloutAccessoryView;
+    imageView.image = [UIImage imageNamed:@"camera"];
+
+    return annotationView;
 }
 
 #pragma mark - Navigation
